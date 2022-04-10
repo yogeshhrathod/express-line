be58ea74e7f348de35cbba90712c82710eb7e9ef
yoggi <yrathod33@gmail.com>
2022-04-10 14:42:30 +0530

    feat: added configuration

--- index.ts
+++ index.ts
@@ -1,27 +1,21 @@
-import * as express from "express";
-import * as logging from "./config/logger"
-import * as winston from "winston";
-import * as expressWinston from "express-winston"
+import * as express from 'express';
+
+import router from './router';
+import { Logger, APILogger } from './config/Logger';
 
 const app = express();
-const port = 3000
-app.use(expressWinston.logger({
-    transports: [
-      new winston.transports.Console()
-    ],
-    format: winston.format.combine(
-    //   winston.format.colorize(),
-      winston.format.json()
-    ),
-    meta: false,
-    msg: "HTTP  ",
-    expressFormat: true,
-    colorize: false,
-    ignoreRoute: function (req, res) { return false; }
-  }));
-  app.get('/', (req, res) => {
-      res.send("Hello World!");
-  });
+const port = 3000;
+
+app.use(APILogger);
+
+app.use('/v1', router);
+
+app.get('/', (req, res) => {
+  res.send(`UPTIME -> ${(Date.now() - serverStart) / 1000} seconds`);
+});
+
+const serverStart: number = Date.now();
+
 app.listen(port, () => {
-    console.log(`Timezones by location application is running on port ${port}.`);
+  Logger.info(`Server is running on port ${port}.✔`);
 });
